ea7c1872eada591c73a7d0caa781776bbfb111b4
Kristof Dewilde <code@qrivi.dev>
2024-02-13 01:07:04 +0100

    Double click for your convenience

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ const render = () => {
   window.requestAnimationFrame(render);
 };
 
+const duplicate = () => {
+  const randomX = Math.floor(Math.random() * window.screen.availWidth) - 400;
+  const randomY = Math.floor(Math.random() * window.screen.availHeight) - 400;
+
+  window.open(window.location.href, "_blank", `resizable=yes,width=400,height=400,left=${randomX},top=${randomY}`);
+};
+
 navigator.mediaDevices
   .getUserMedia({ video: true })
   .then((stream) => {
     video.srcObject = stream;
     video.width = window.screen.availWidth;
     video.height = window.screen.availHeight;
+    video.addEventListener("dblclick", duplicate);
 
     window.requestAnimationFrame(render);
   })
